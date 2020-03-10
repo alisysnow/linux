22d9e2e68f76680ca63015b7335269a457935803
alisysnow <61377643+alisysnow@users.noreply.github.com>
2020-03-10 06:40:00 +0000

    Create vfio-pci-override.sh

---
+++ vfio-pci-override.sh
@@ -0,0 +1,8 @@
+#!xD
+PREREQS=""
+DEVS="0000:01:00.0 0000:01:00.1 0000:04:00.0"
+for DEV in $DEVS; do
+        echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+done
+
+modprobe -i vfio-pci
